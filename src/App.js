eb003320b198c621d720c62386ef4794e827a0c2
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-10-13 16:12:31 +0530

    sorting-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+// Now in App.js, we will return these two components under row flex. Now we will create state variables that need to be passed to the Sidebar & Visualizer components. Create an arr variable to store numbers that need to be sorted, generating (boolean variable to check whether a new array is generating or not), sorting (boolean variable to check whether an array is soring or not).
 import './App.css';
+import { Box, Flex} from "@chakra-ui/react";
+import { Sidebar } from "./components/Sidebar";
+import { Visualizer } from "./components/Visualizer";
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [arr, setArr] = useState([]);
+  const [generating, setGenerating] = useState(false);
+  const [sorting, setSorting] = useState(false);
+
+  const generateArr = (formState) => {
+    let newArr = [];
+    while (newArr.length <= formState.size) {
+      let random = Math.floor(Math.random() * (200 - 10) + 10);
+      if (newArr.indexOf(random) === -1) {
+        newArr.push(random);
+      }
+    }
+    setArr(...newArr);
+  }
+  useState(() => {
+    generateArr({ size: 10 });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box p={"4"}>
+      <Flex gap={"4"}>
+        <Sidebar
+          generateArr={generateArr}
+
+          // sort={sort}
+        />
+        <Visualizer data={arr} />
+      </Flex>
+    </Box>
+    </>
   );
 }
 
